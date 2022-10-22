7ce436e5a42d45923e5a11c4d577cf50424f8292
alifdoll <alifianfajarpratama@gmail.com>
2022-10-22 22:20:58 +0700

    update

--- public/index.php
+++ public/index.php
@@ -38,7 +38,7 @@ if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php'
 require __DIR__ . '/../vendor/autoload.php';
 
 // UTK SERVER
-// require __DIR__ . '/../repositories/story-app-api/vendor/autoload.php';
+// require __DIR__ . '/../../repositories/story-app-api/vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -54,7 +54,7 @@ require __DIR__ . '/../vendor/autoload.php';
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
 // UTK SERVER
-// $app = require_once __DIR__ . '/../repositories/story-app-api/bootstrap/app.php';
+// $app = require_once __DIR__ . '/../../repositories/story-app-api/bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
 
