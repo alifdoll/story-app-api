858ca1263f11a6d6becedca2c95a579e6ac83551
alifdoll <alifianfajarpratama@gmail.com>
2022-10-23 15:40:35 +0700

    test

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/test', function () {
-    $targetFolder = $_SERVER['DOCUMENT_ROOT'] . '/storage/app/public';
+    $targetFolder = $_SERVER['DOCUMENT_ROOT'] . 'repositories/story-app-api/storage/app/public';
     $linkFolder = $_SERVER['DOCUMENT_ROOT'] . '/public/storage';
     // symlink($targetFolder, $linkFolder);
     // echo 'Symlink process successfully completed';
