6d00a1bc810706b040548bc9bc5ed044e22884c2
alifdoll <alifianfajarpratama@gmail.com>
2022-10-23 15:39:34 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/test', function () {
-    Artisan::call('storage:link');
+    $targetFolder = $_SERVER['DOCUMENT_ROOT'] . '/storage/app/public';
+    $linkFolder = $_SERVER['DOCUMENT_ROOT'] . '/public/storage';
+    // symlink($targetFolder, $linkFolder);
+    // echo 'Symlink process successfully completed';
+    echo $targetFolder;
 });
 
 Route::get('/', function () {
